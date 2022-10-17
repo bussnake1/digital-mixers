7561a39e0701bd7dff2d7e52a48d0cea1ef46f36
Daniel Pocs <bussnake@Daniels-MacBook-Air.local>
2022-10-17 22:05:07 +0200

    chore: simple usefetch hook for fetching data and handling loading state

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+
+export default function useFetch<DataType>(url: string): {
+  loading: boolean
+  data: DataType
+  error: Error
+} {
+  const [loading, setLoading] = useState(true)
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [data, setData] = useState<DataType>([] as DataType)
+  const [error, setError] = useState<Error>(null as unknown as Error)
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data)
+        setLoading(false)
+      })
+      .catch((error) => {
+        console.error(error)
+        setError(error)
+      })
+  }, [url])
+
+  return {
+    loading,
+    data,
+    error
+  }
+}
