3ef52e1d2a8400c3456ff22dff682eccff003376
Daniel Pocs <bussnake@Daniels-MacBook-Air.local>
2022-10-17 22:02:26 +0200

    chore: tw and vite config update for fonts and line-clamp

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./dist/**/*.html', './src/**/*.{js,jsx,ts,tsx}', './*.html'],
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/line-clamp')],
+  theme: {
+    extend: {
+      colors: {
+        offwhite: '#F5F5F5'
+      },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        futura: ['Futura', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
   variants: {
     extend: {
       opacity: ['disabled']
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePluginFonts } from 'vite-plugin-fonts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePluginFonts({
+      google: {
+        families: ['Inter']
+      },
+      custom: {
+        families: [
+          {
+            name: 'Futura',
+            local: 'Futura',
+            src: './assets/fonts/Futura.ttf'
+          }
+        ]
+      }
+    })
+  ]
 })
