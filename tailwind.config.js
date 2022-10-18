5cac335bcce5445723d9a960e64ca86e31eb0db1
Daniel Pocs <bussnake@Daniels-MacBook-Air.local>
2022-10-18 10:48:41 +0200

    chore: skyggen shadow in tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,20 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        offwhite: '#F5F5F5'
+        offwhite: '#F5F5F5',
+        opacity20: 'rgba(255, 255, 255, 0.2)'
       },
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
         futura: ['Futura', ...defaultTheme.fontFamily.sans]
+      },
+      dropShadow: {
+        megetSkyggen: [
+          '0px 33px 60px rgba(41, 40, 98, 0.08)',
+          '0px 16.7062px 26.1562px rgba(41, 40, 98, 0.054)',
+          '0px 6.6px 9.75px rgba(41, 40, 98, 0.04)',
+          '0px 1.44375px 3.46875px rgba(41, 40, 98, 0.026)'
+        ]
       }
     }
   },
