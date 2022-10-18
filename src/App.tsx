ebfa204a66192953c154b77ff696d55e7e972610
Daniel Pocs <bussnake@Daniels-MacBook-Air.local>
2022-10-18 09:02:30 +0200

    chore: updated app to use cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React, { ReactElement, useState, useEffect } from 'react'
 import { getCocktailUrl } from './api/api'
 import { Card } from './components/Card'
+import { DetailCard } from './components/DetailCard'
 
 import useFetch from './hooks/useFetch'
 import { Cocktail, Cocktails } from './types'
 
 function App(): ReactElement {
-  const [selectedCocktail, setSelectedCocktail] = useState(null)
+  const [selectedCocktail, setSelectedCocktail] = useState<null | Cocktail>(
+    null
+  )
+  const [selectedIndex, setSelectedIndex] = useState<number>(0)
 
   const {
     data: cocktails,
@@ -14,7 +18,18 @@ function App(): ReactElement {
     error
   } = useFetch<Cocktails>(getCocktailUrl({ search: 'A' }))
 
-  console.log(cocktails)
+  // console.log(cocktails)
+
+  const onCardClick = (cocktail: Cocktail, index: number) => {
+    if (selectedCocktail) return
+    setSelectedCocktail(cocktail)
+    setSelectedIndex(index)
+  }
+
+  const isEverySecondPair = (i: number) => i % 4 === 0 || i % 4 === 1
+  const isLeftSide = (i: number) => isEverySecondPair(i)
+  const hideCard = (cardIndex: number, selectedIndex: number) =>
+    isEverySecondPair(cardIndex) === isEverySecondPair(selectedIndex)
 
   if (loading) return <p>Loading..</p>
   if (error) return <p>Error..</p>
@@ -23,17 +38,41 @@ function App(): ReactElement {
     <div className="px-10 py-[86px] bg-offwhite">
       <header className="font-futura">
         <p className="flex items-center justify-center w-full leading-normal text-2xl">
-          Welcome to
+          {selectedCocktail ? 'You have selected' : 'Welcome to'}
         </p>
         <h1 className="flex items-center justify-center w-full font-bold text-5xl leading-normal">
-          Digital Mixers
+          {selectedCocktail
+            ? selectedCocktail?.strDrink || 'A drink'
+            : 'Digital Mixers'}
         </h1>
       </header>
-      <main className="pt-[103px]">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10">
-          {cocktails.drinks.slice(0, 8).map((cocktail: Cocktail) => (
-            <Card cocktail={cocktail} key={cocktail.idDrink} />
+      <main className="pt-[103px] flex space-x-10">
+        <div
+          className={`grid gap-10 w-full grid-cols-1 sm:grid-cols-2 md:grid-cols-4 ${
+            selectedCocktail ? 'grid-flow-col' : ''
+          }`}
+        >
+          {cocktails.drinks.slice(0, 8).map((cocktail: Cocktail, i) => (
+            <Card
+              cocktail={cocktail}
+              key={cocktail.idDrink}
+              onClick={() => onCardClick(cocktail, i)}
+              className={
+                selectedCocktail && hideCard(i, selectedIndex) ? 'hidden' : ''
+              }
+            />
           ))}
+          {selectedCocktail && (
+            <DetailCard
+              cocktail={selectedCocktail}
+              className={
+                selectedCocktail
+                  ? `block ${isLeftSide(selectedIndex) ? 'order-first' : ''}`
+                  : 'hidden'
+              }
+              close={() => setSelectedCocktail(null)}
+            />
+          )}
         </div>
       </main>
     </div>
