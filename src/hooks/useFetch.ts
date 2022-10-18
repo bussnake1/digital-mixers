b30b6d0b627441a536d5abb700f453136edb7ac3
Daniel Pocs <bussnake@Daniels-MacBook-Air.local>
2022-10-18 10:49:54 +0200

    chore: fixed some type cast error on build

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -8,7 +8,7 @@ export default function useFetch<DataType>(url: string): {
   const [loading, setLoading] = useState(true)
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const [data, setData] = useState<DataType>([] as DataType)
+  const [data, setData] = useState<DataType>([] as unknown as DataType)
   const [error, setError] = useState<Error>(null as unknown as Error)
 
   useEffect(() => {
